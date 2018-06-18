6263afee39efde923417bad56317ecc6fc86e7e4
Daniel Muysken <daniel@wcw-stud-145-109-43-121.wireless.uva.nl>
2018-06-18 14:47:53 +0200

    added penalties for the heads being together

--- micelle_lattice_v3.py
+++ micelle_lattice_v3.py
@@ -2,89 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-lattice = np.zeros((40, 40, 100000))
-T = .1
+timesteps = 1000000
+
+lattice = np.zeros((40, 40, timesteps))
+
+T = np.zeros(timesteps)
+T[0] = .1
 
 for i in range(300):
     lattice[np.random.randint(1, 39), np.random.randint(1, 39), 0] = np.random.randint(1, 5)
 
-def energy_f(lat, x, y):
-    if lat[x, y] == 0:
-        a, b = 0, 0
-    elif lat[x, y] == 1:
-        if lat[x+1, y] == 2 or lat[x+1, y] == 4:
-            a = -1.
-        elif lat[x+1, y] == 0:
-            a = 0.
-        else:
-            a = 1.
-        if lat[x, y-1] == 3 or lat[x, y-1] == 4:
-            b = -1.
-        elif lat[x, y-1] == 0:
-            b = 0.
-        else:
-            b = 1.
-    elif lat[x, y] == 2:
-        if lat[x-1, y] == 1 or lat[x-1, y] == 3:
-            a = -1.
-        elif lat[x-1, y] == 0:
-            a = 0.
-        else:
-            a = 1.
-        if lat[x, y-1] == 3 or lat[x, y-1] == 4:
-            b = -1.
-        elif lat[x, y-1] == 0:
-            b = 0.
-        else:
-            b = 1.
-    elif lat[x, y] == 3:
-        if lat[x+1, y] == 2 or lat[x+1, y] == 4:
-            a = -1.
-        elif lat[x+1, y] == 0:
-            a = 0.
-        else:
-            a = 1.
-        if lat[x, y+1] == 1 or lat[x, y+1] == 2:
-            b = -1.
-        elif lat[x, y+1] == 0:
-            b = 0.
-        else:
-            b = 1.
-    elif lat[x, y] == 4:
-        if lat[x-1, y] == 1 or lat[x-1, y] == 3:
-            a = -1.
-        elif lat[x-1, y] == 0:
-            a = 0.
-        else:
-            a = 1.
-        if lat[x, y+1] == 1 or lat[x, y+1] == 2:
-            b = -1.
-        elif lat[x, y+1] == 0:
-            b = 0.
-        else:
-            b = 1.
-    return a + b
 
-def energy_f2(lat):
-    H_lat = (-1. * np.logical_or(
-                     np.logical_and(np.logical_or(lat[1:39, 1:39] == 1, lat[1:39, 1:39] == 3), np.logical_or(lat[2:40, 1:39] == 2, lat[2:40, 1:39] == 4)), 
-                     np.logical_and(np.logical_or(lat[1:39, 1:39] == 2, lat[1:39, 1:39] == 4), np.logical_or(lat[0:38, 1:39] == 1, lat[0:38, 1:39] == 3))
-                   ) +
-             1. * np.logical_or(
-                     np.logical_and(np.logical_or(lat[1:39, 1:39] == 1, lat[1:39, 1:39] == 3), np.logical_or(lat[2:40, 1:39] == 1, lat[2:40, 1:39] == 3)), 
-                     np.logical_and(np.logical_or(lat[1:39, 1:39] == 2, lat[1:39, 1:39] == 4), np.logical_or(lat[0:38, 1:39] == 2, lat[0:38, 1:39] == 4))
-                  ) + 
-             -1. * np.logical_or(
-                     np.logical_and(np.logical_or(lat[1:39, 1:39] == 1, lat[1:39, 1:39] == 2), np.logical_or(lat[1:39, 0:38] == 3, lat[1:39, 0:38] == 4)),
-                     np.logical_and(np.logical_or(lat[1:39, 1:39] == 3, lat[1:39, 1:39] == 4), np.logical_or(lat[1:39, 2:40] == 1, lat[1:39, 2:40] == 2))
-                   ) + 
-             1. * np.logical_or(
-                     np.logical_and(np.logical_or(lat[1:39, 1:39] == 1, lat[1:39, 1:39] == 2), np.logical_or(lat[1:39, 0:38] == 1, lat[1:39, 0:38] == 2)),
-                     np.logical_and(np.logical_or(lat[1:39, 1:39] == 3, lat[1:39, 1:39] == 4), np.logical_or(lat[1:39, 2:40] == 3, lat[1:39, 2:40] == 4))
-                  ))
-    return np.sum(H_lat)
-
-def energy_f3(lat):
+def energy_f(lat):
     H_lat = (-1. * (
                      ((lat[1:39, 1:39]==1) + (lat[1:39, 1:39]==3))*((lat[2:40, 1:39]==2) + (lat[2:40, 1:39]==4)) + 
                      ((lat[1:39, 1:39]==2) + (lat[1:39, 1:39]==4))*((lat[0:38, 1:39]==1) + (lat[0:38, 1:39]==3))
@@ -100,13 +29,30 @@ def energy_f3(lat):
              1. * (
                      ((lat[1:39, 1:39]==1) + (lat[1:39, 1:39]==2))*((lat[1:39, 0:38]==1) + (lat[1:39, 0:38]==2)) +
                      ((lat[1:39, 1:39]==3) + (lat[1:39, 1:39]==4))*((lat[1:39, 2:40]==3) + (lat[1:39, 2:40]==4))
+                  ) +
+             -1. * (
+                     ((lat[1:39, 1:39]==1) + (lat[1:39, 1:39]==3))*(lat[0:38, 1:39]==0) + 
+                     ((lat[1:39, 1:39]==2) + (lat[1:39, 1:39]==4))*(lat[2:40, 1:39]==0)
+                   ) + 
+             1. * (
+                     ((lat[1:39, 1:39]==1) + (lat[1:39, 1:39]==3))*((lat[0:38, 1:39]==1) + (lat[0:38, 1:39]==3)) + 
+                     ((lat[1:39, 1:39]==2) + (lat[1:39, 1:39]==4))*((lat[2:40, 1:39]==2) + (lat[2:40, 1:39]==4))
+                  ) +
+             -1. * (
+                     ((lat[1:39, 1:39]==1) + (lat[1:39, 1:39]==2))*(lat[1:39, 2:40]==0) +
+                     ((lat[1:39, 1:39]==3) + (lat[1:39, 1:39]==4))*(lat[1:39, 0:38]==0)
+                   ) + 
+             1. * (
+                     ((lat[1:39, 1:39]==1) + (lat[1:39, 1:39]==2))*((lat[1:39, 2:40]==1) + (lat[1:39, 2:40]==2)) +
+                     ((lat[1:39, 1:39]==3) + (lat[1:39, 1:39]==4))*((lat[1:39, 0:38]==3) + (lat[1:39, 0:38]==4))
                   ))
     return np.sum(H_lat)
 
-energy = np.zeros(100000)
-energy[0] = energy_f3(lattice[:, :, 0])
 
-for n in range(1, 100000):
+energy = np.zeros(timesteps)
+energy[0] = energy_f(lattice[:, :, 0])
+
+for n in range(1, timesteps):
     lattice[:, :, n] = lattice[:, :, n-1]
     while True:
         i, j = np.random.randint(1, 39), np.random.randint(1, 39)
@@ -120,39 +66,55 @@ for n in range(1, 100000):
             if 0 < i+di < 39 and 0 < j+dj < 39:
                 break
         lattice[i, j, n], lattice[i+di, j+dj, n] = lattice[i+di, j+dj, n-1], lattice[i, j, n-1]
-    energy[n] = energy_f3(lattice[:, :, n])
-    if np.random.random() >= np.e**((energy[n-1] - energy[n])/T): # rejecting change
+    energy[n] = energy_f(lattice[:, :, n])
+    T[n] = T[n-1] #- 1./timesteps
+    if np.random.random() >= np.e**((energy[n-1] - energy[n])/T[n]): # rejecting change
 	    lattice[:, :, n] = lattice[:, :, n-1]
 	    energy[n] = energy[n-1]
 	    
 
 print "simulation done"
-print sum(energy_f(lattice[:, :, -1], x, y) for x in range(1, 39) for y in range(1, 39))
-print energy_f2(lattice[:, :, -1])
-print energy_f3(lattice[:, :, -1])
+print energy_f(lattice[:, :, -1])
 
-plt.figure(0, figsize=(6,6))
-lns = plt.plot(np.arange(100000), energy, "-")
-plt.xlabel("t")
-plt.ylabel("H")
+# plt.figure(0, figsize=(6,6))
+# lns = plt.plot(np.arange(timesteps), energy, "-")
+# plt.xlabel("t")
+# plt.ylabel("H")
+# plt.savefig('energy_plot.png')
+# plt.close(0)
+
+fig, ax1 = plt.subplots()
+
+color = 'red'
+ax1.set_xlabel('time')
+ax1.set_ylabel('H', color=color)
+ax1.plot(np.arange(timesteps), energy, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+color = 'blue'
+ax2.set_ylabel('temperature', color=color)  # we already handled the x-label with ax1
+ax2.plot(np.arange(timesteps), T, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.savefig('energy_plot.png')
-plt.close(0)
 
-
+scale = timesteps/1000
 for n in range(1000):  
     plt.figure(n + 2, figsize=(6,6))
     lns = plt.plot()
     plt.axis([0, 41, 0, 41])
-    plt.title("t=%05d" % (n*100))
+    plt.title("t=%d" % (n*scale))
     for i in range(40):
         for j in range(40):
-            if lattice[i, j, n*100] == 1:
+            if lattice[i, j, n*scale] == 1:
                 plt.arrow(i, j+1, 0.8, -0.8, length_includes_head=True, head_width=0.3)
-            elif lattice[i, j, n*100] == 2:
+            elif lattice[i, j, n*scale] == 2:
                 plt.arrow(i+1, j+1, -0.8, -0.8, length_includes_head=True, head_width=0.3)
-            elif lattice[i, j, n*100] == 3:
+            elif lattice[i, j, n*scale] == 3:
                 plt.arrow(i, j, 0.8, 0.8, length_includes_head=True, head_width=0.3)
-            elif lattice[i, j, n*100] == 4:
+            elif lattice[i, j, n*scale] == 4:
                 plt.arrow(i+1, j, -0.8, 0.8, length_includes_head=True, head_width=0.3)
     plt.savefig('lattice_plots/lattice_%03d.png' % n)
     plt.close(n)
