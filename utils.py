2d2e877bf64faf1b995a7edffafd13e9d01970f9
eathieniti <efi.athieniti@gmail.com>
2018-06-20 18:44:54 +0200

    updated utils to not update the -1 empty sites if they are already zeroed also updated the fname to the full name of the file

--- utils.py
+++ utils.py
@@ -3,33 +3,74 @@ __author__ = 'efiathieniti'
 import numpy as np
 from pylab import *
 from scipy.ndimage import measurements
+import copy
 
-
-
-def calculate_sizes(lattice, fname):
+def calculate_sizes_zero_for_empty(lattice_snapshot, fname, loglog_scale=None):
     #
     # Calculate degree distribution for the final configuration
     #
 
-    final_lattice = lattice
+
     #print(final_lattice)
 
-    final_lattice[final_lattice!=-1]=1
-    final_lattice[final_lattice==-1]=0
-    lw, num = measurements.label(final_lattice)
+    ls = lattice_snapshot.copy()
+    if -1 in ls:
+        ls[ls!=-1]=1
+        ls[ls==-1]=0
+    lw, num = measurements.label(ls)
 
 
     plt.figure()
     lns = plt.plot()
     plt.title("size distribution")
-    area = measurements.sum(final_lattice, lw, index=arange(lw.max() + 1))
+    area = measurements.sum(ls, lw, index=arange(lw.max() + 1))
+    area = area[area>1]
     plt.hist(area, bins=100)
     average_object_size = np.mean(area)
     median_object_size = np.median(area)
     print("average object size = ", average_object_size)
     print("median object size = ", median_object_size)
+    if loglog_scale:
+            plt.xscale('log')
+            plt.yscale('log')
 
-    plt.savefig('%s.png'%fname)
+
+    plt.savefig(fname)
+
+
+
+
+
+def calculate_sizes(lattice_snapshot, fname, loglog=None):
+    #
+    # Calculate degree distribution for the final configuration
+    #
+
+
+    #print(final_lattice)
+    ls = lattice_snapshot.copy()
+    ls[ls!=-1]=1
+    ls[ls==-1]=0
+    lw, num = measurements.label(ls)
+
+
+    plt.figure()
+    lns = plt.plot()
+    plt.title("size distribution")
+    area = measurements.sum(ls, lw, index=arange(lw.max() + 1))
+    area = area[area>1]
+    plt.hist(area, bins=100)
+    average_object_size = np.mean(area)
+    median_object_size = np.median(area)
+    print("average object size = ", average_object_size)
+    print("median object size = ", median_object_size)
+    if loglog:
+            plt.xscale('log')
+            plt.yscale('log')
+
+
+    plt.savefig(fname)
+
 
 #lattice = np.load('lattice_100000_x_100_y_100.npy')
 
