9c7df6b3e6ece3ba23e868a7ae69c45fa231e8a9
DanielMuysken <daniel@wcw-stud-145-109-45-185.wireless.uva.nl>
2018-06-22 13:21:57 +0200

    directional movement

--- continuous_model.py
+++ continuous_model.py
@@ -7,9 +7,9 @@ from scipy.optimize import minimize
 lip_no = 100
 dimensions = 20
 T = 0.1
-N = 100
+N = 200
 
-dxys = norm.rvs(size=(2, lip_no, N))
+noise = norm.rvs(size=(2, lip_no, N)) * 0.01
 
 lip_heads = np.zeros((2, lip_no, N))
 lip_tails = np.zeros((2, lip_no, N))
@@ -23,25 +23,39 @@ for i in range(lip_no):
     lip_tails[0, i, 0] = lip_heads[0, i, 0] + 0.4*np.cos(rand * 2 * np.pi)
     lip_tails[1, i, 0] = lip_heads[1, i, 0] + 0.4*np.sin(rand * 2 * np.pi)
 
-def dist_constraint(pos1, x2, y2):
-    return ((pos1[0]-x2)**2 + (pos1[1]-y2)**2)**0.5 - 0.4
+def dist_constraint(pos):
+    """
+    This is the constraint to keep the distance between the head and the tail constant.
+    """
+    return ((pos[0]-pos[2])**2 + (pos[1]-pos[3])**2)**0.5 - 0.4
+
+def small_step_constraint(pos, x_old, y_old):
+    """
+    This is the constraint for not making too large steps.
+    """
+    return -(((x_old - (pos[0]+pos[2])/2.)**2 + (y_old - (pos[1]+pos[3])/2.)**2)**0.5 - 0.2)
 
 # def jac_dist_constraint(pos1, x2, y2):
 #     return ()
     
-def distance(pos, others): 
-    return np.sum(np.sum((others.T - pos)**2, axis=1)**0.5, axis=0)
+def distance(pos, tails, heads): 
+    return (np.sum(np.sum((tails.T - pos[2:])**2, axis=1)**0.5, axis=0) - 
+            0.1 * np.sum(np.sum((heads.T - pos[:2])**2, axis=1)**0.5, axis=0))
 
 a = np.zeros((lip_no, N))
 for n in range(1, N):
-	lip_heads[:, :, n] = np.clip(lip_heads[:, :, n-1] + dxys[:, :, n] * a[:, n-1], 1, dimensions-1)
-	lip_tails[:, :, n] = np.clip(lip_tails[:, :, n-1] + dxys[:, :, n] * a[:, n-1], 1, dimensions-1)
-	for i in range(lip_no):
-		neighbours = lip_tails[:, ((lip_tails[0, :, n] - lip_tails[0, i, n])**2 + (lip_tails[1, :, n] - lip_tails[1, i, n])**2)**0.5 < 5, n]
- 		res = minimize(distance, lip_tails[:, i, n], args=(neighbours),
- 		               constraints={"type": "eq", "fun": dist_constraint, "args": lip_heads[:, i, n]})
- 		lip_tails[:, i, n] = res.x
- 		a[i, n] = res.fun / (neighbours.shape[1]*10)
+	lip_heads[:, :, n] = lip_heads[:, :, n-1]
+	lip_tails[:, :, n] = lip_tails[:, :, n-1]
+	for i in np.random.permutation(lip_no):
+		neighbours = np.array(((lip_tails[0, :, n] - lip_tails[0, i, n])**2 + (lip_tails[1, :, n] - lip_tails[1, i, n])**2)**0.5 < 5.)
+ 		masspoint = ((lip_heads[0, i, n] + lip_tails[0, i, n])/2., (lip_heads[1, i, n] + lip_tails[1, i, n])/2.)
+ 		res = minimize(distance, np.concatenate((lip_tails[:, i, n], lip_heads[:, i, n])), 
+ 		               args=(lip_tails[:, neighbours, n], lip_heads[:, neighbours, n]),
+ 		               constraints=({"type": "eq", "fun": dist_constraint}, 
+ 		                            {"type": "ineq", "fun": small_step_constraint, "args": masspoint}))
+ 		new_pos = res.x
+ 		lip_tails[:, i, n] = np.clip(new_pos[2:] + noise[:, i, n], 1, dimensions-1)
+ 		lip_heads[:, i, n] = np.clip(new_pos[:2] + noise[:, i, n], 1, dimensions-1)
  	print n
         
 
