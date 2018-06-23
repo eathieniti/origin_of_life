634308c8207d934ee50245e8721194738b65b175
DanielMuysken <daniel@macbook-air-2.home>
2018-06-23 10:09:14 +0200

    parallel visualization

--- continuous_model.py
+++ continuous_model.py
@@ -1,13 +1,18 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from math import sqrt
+
+from multiprocessing import Process
+
 from scipy.stats import norm
 from scipy.optimize import minimize
 
-lip_no = 100
+lip_no = 200
 dimensions = 20
 T = 0.1
-N = 200
+N = 300
 
 dir_step_size = 0.1
 noise_scale = 0.1
@@ -15,7 +20,7 @@ infl_repulsion_heads = 0.2
 detection_radius = 5.
 lipid_length = 0.4
 lip_len_scale = 0.
-min_tail_dist = 0.001
+min_tail_dist = 0.1
 
 noise = norm.rvs(size=(2, lip_no, N)) * noise_scale
 
@@ -60,15 +65,28 @@ def distance(pos, tails, heads):
     return (np.sum(np.sum((tails.T - pos[:2])**2, axis=1)**0.5, axis=0) - 
             infl_repulsion_heads * np.sum(np.sum((heads.T - pos[2:])**2, axis=1)**0.5, axis=0))
 
+
+def visualization(i):
+    plt.figure(i, figsize=(6,6))
+    axes = plt.gca()
+    axes.set_xlim([0,dimensions])
+    axes.set_ylim([0,dimensions])
+    plt.plot([lip_tails[0,:,i], lip_heads[0,:,i]], [lip_tails[1,:,i], lip_heads[1,:,i]], 'k-')
+    for j in range(lip_no):
+        plt.plot(lip_tails[0,j,i], lip_tails[1,j,i], 'r.')
+    plt.savefig('plots/continuous_%03d.png'%(i))
+    #plt.show()
+    plt.close(i)
+
 a = np.zeros((lip_no, N))
 for n in range(1, N):
     lip_heads[:, :, n] = lip_heads[:, :, n-1]
     lip_tails[:, :, n] = lip_tails[:, :, n-1]
     for i in np.random.permutation(lip_no):
         neighbours = np.array(((lip_tails[0, :, n] - lip_tails[0, i, n])**2 + (lip_tails[1, :, n] - lip_tails[1, i, n])**2)**0.5 < detection_radius)
-        
+        if sum(neighbours) == 0:
+            continue
         masspoint = ((lip_heads[0, i, n] + lip_tails[0, i, n])/2., (lip_heads[1, i, n] + lip_tails[1, i, n])/2.)
-        
         res = minimize(distance, np.concatenate((lip_tails[:, i, n], lip_heads[:, i, n])), 
                        args=(lip_tails[:, neighbours, n], lip_heads[:, neighbours, n]),
                        constraints=({"type": "eq", "fun": dist_constraint, "args": (lip_lengths[i],)}, 
@@ -78,17 +96,11 @@ for n in range(1, N):
         lip_tails[:, i, n] = np.clip(new_pos[:2] + noise[:, i, n], 1, dimensions-1)
         lip_heads[:, i, n] = np.clip(new_pos[2:] + noise[:, i, n], 1, dimensions-1)
     print n
+    p = Process(target=visualization, args=(n,))
+    p.start()
+    p.join()
+    #visualization(n)
         
 
-for i in range(1,N):
-    plt.figure(i-1, figsize=(6,6))
-    axes = plt.gca()
-    axes.set_xlim([0,dimensions])
-    axes.set_ylim([0,dimensions])
-    plt.plot([lip_tails[0,:,i], lip_heads[0,:,i]], [lip_tails[1,:,i], lip_heads[1,:,i]], 'k-')
-
-    for j in range(lip_no):
-        plt.plot(lip_tails[0,j,i], lip_tails[1,j,i], 'r.')
-    plt.savefig('plots/continuous_%03d.png'%(i))
-    #plt.show()
-    plt.close(i-1)
+# for i in range(N):
+#     visualization(i)
